525a14b8b76508256c1e6afb3a87dd5278988d5c
hafid759840 <hafidxvenszy@gmail.com>
2019-08-31 12:46:53 +0700

    Temaku

---
+++ app/src/main/java/com/example/aplikasiku/NotificationFragment.kt
@@ -0,0 +1,15 @@
+package com.example.aplikasiku
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+public class NotificationFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_notification, container, false)
+}
